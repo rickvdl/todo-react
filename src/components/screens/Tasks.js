16a79574c3d9c38cd8febc62ff8672be01cf3776
Rick van der Linden <rickvdl@me.com>
2018-10-20 21:57:30 +0200

    Added react-router, created fake screens

---
+++ src/components/screens/Tasks.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import {
+  withRouter
+} from 'react-router-dom'
+
+class Tasks extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Tasks</h1>
+      </div>
+    )
+  }
+}
+
+export default withRouter(Tasks)
