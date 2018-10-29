1e048559d08d1a7e4111901e548af9d0d055fb74
Rick van der Linden <rickvdl@me.com>
2018-10-29 14:32:38 +0100

    Added localstorage for token

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,7 +1,7 @@
 import { LOGIN, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT_SUCCESS, LOGOUT_FAIL, REMOVE_TOKEN } from '../actions'
 
 const initialState = {
-  token: null,
+  token: localStorage.getItem('token') || null,
   loading: false,
   error: null,
 }
@@ -11,14 +11,26 @@ export default (state = initialState, action: Object) => {
   case LOGIN: 
     return {...state}
   case LOGIN_SUCCESS:
-    return {...state, token: action.payload.data.token}
+    const { token } = action.payload.data
+    setToken(token)
+    return {...state, token}
   case LOGIN_FAIL:
+    removeToken()
     return {...state, error: action.error.message}
   case LOGOUT_SUCCESS:
   case REMOVE_TOKEN:
   case LOGOUT_FAIL:
+    removeToken()
     return {...state, token: null}
   default:
     return state
   }
+}
+
+const setToken = (token) => {
+  localStorage.setItem('token', token)
+}
+
+const removeToken = () => {
+  setToken(null)
 }
